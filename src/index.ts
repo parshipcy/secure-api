57b1e3405bdac45178b118582483a05e46fa0a70
Parship Chowdhury <parshipchowdhury@gmail.com>
2026-09-03 23:12:17 +0530

    chore: create server and configure environment variables
    Signed-off-by: Parship Chowdhury <parshipchowdhury@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello")
+import express, {Express, Request, Response} from "express"
+import {config} from "dotenv"
+
+const app: Express = express()
+config()
+const port = process.env.PORT || 8080
+
+app.get("/", (req: Request, res: Response) => {
+    res.json({ success: true, message: "Hello world"})
+})
+
+app.listen(port,() => console.log(`Server running on port ${port}`))
