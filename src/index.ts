a463f342a4ac2640023be996f523605a135323d4
Parship Chowdhury <parshipchowdhury@gmail.com>
2026-09-04 13:48:54 +0530

    chore: setting cors and middlewares
    Signed-off-by: Parship Chowdhury <parshipchowdhury@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import express, {Express, Request, Response} from "express"
 import {config} from "dotenv"
+import cors from "cors"
 
 const app: Express = express()
+
 config()
 const port = process.env.PORT || 8080
+
+//middlewares
+app.use(cors({
+    origin: process.env.HOST_URL || "*"
+}))
 
 app.get("/", (req: Request, res: Response) => {
     res.json({ success: true, message: "Hello world"})
