a9641dc708e70714c1342fa1bb2e369c4e89e9d2
Parship Chowdhury <parshipchowdhury@gmail.com>
2026-09-05 12:41:33 +0530

    chore: setting up mongodb
    Signed-off-by: Parship Chowdhury <parshipchowdhury@gmail.com>

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -2,11 +2,11 @@ import mongoose from "mongoose"
 
 const dbConnect: () => Promise<void> = async () => {
     try {
-        // await mongoose.connect(process.env.MONGO_URI as string)
-        // console.log("Connected to DB")
+        await mongoose.connect(process.env.MONGO_URI as string)
+        console.log("Connected to DB")
     } catch(error: any){
-        // console.log("Error connecting to DB", error.message)
-        // process.exit(1)
+        console.log("Error connecting to DB", error.message)
+        process.exit(1)
     }
 }
 
